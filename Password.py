5de6d10d97c9ec8dbb4ddbf9aa52dcb11a59afeb
raphael-rossignol <raphael.rossignol@laplateforme.io>
2023-01-31 13:17:55 +0100

    Main part of the job done

---
+++ Password.py
@@ -0,0 +1,63 @@
+def user_input():
+    valid_car_spe = "!@#$%^&*()_-+={}[]"
+    list_password = list(input("Choose a password, it must contain 1 Upper, 1 Low, 1 Digit, 1 Special Character and 8 minimum"))
+    size_password = len(list_password)
+
+    if size_password >= 7:      # Check if the input's length is right
+
+        for space in list_password:     # Check if there's no space
+            space = space.isspace()
+            if space == 0:
+                print("Space OK")
+                break
+            elif space == 0:
+                continue
+            else:
+                print("Space not valid, try again")
+                user_input()
+
+        for maj in list_password:       # Check if there's at least 1 Maj Character
+            maj = maj.isupper()
+            if maj == 1:
+                print("Maj OK")
+                break
+            elif maj == 0:
+                continue
+            else:
+                print("Maj not valid, try again")
+                user_input()
+
+        for low in list_password:       # Check if there's at least 1 Low Character
+            low = low.islower()
+            if low == 1:
+                print("Low OK")
+                break
+            elif low == 0:
+                continue
+            else:
+                print("Low not valid, try again")
+                user_input()
+
+        for digit in list_password:     # Check if there's at least 1 Number
+            digit = digit.isdigit()
+            if digit == 1:
+                print("Digit OK")
+                break
+            elif digit == 0:
+                continue
+            else:
+                print("Digit not valid, try again")
+                user_input()
+
+        if any(car_spe in valid_car_spe for car_spe in list_password):      # Check if there's at least 1 Special Character
+            return print("Good Password")       # If everything's valid, return Good Password
+        else:
+            print("Car_spe not valid, try again")
+            user_input()
+
+    else:       # If the length is not right, try again
+        print("Password not valid, try again")
+        user_input()
+
+
+user_input()
