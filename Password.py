77e81dd878b21f4d6a3deb0d4c9cf0acecd6d906
raphael-rossignol <raphael.rossignol@laplateforme.io>
2023-02-01 15:29:44 +0100

    Reworked job

--- Password.py
+++ Password.py
@@ -1,67 +1,45 @@
 from hashlib import sha256
 import json
 
+valid_car_spe = "!@#$%^&*()_-+={}[]"
+valid_car_low = "azertyuiopqsdfghjklmwxcvbn"
+valid_car_upper = "AZERTYUIOPQSDFGHJKLMWXCVBN"
+valid_digit = "1234567890"
+
 
 def user_input():
-    valid_car_spe = "!@#$%^&*()_-+={}[]"
     list_password = list(input("Choose a password, it must contain 1 Upper, 1 Low, 1 Digit, 1 Special Character and 8 Character minimum"))
     password = str(list_password)
     size_password = len(list_password)
 
     if size_password >= 7:      # Check if the input's length is right
 
-        for space in list_password:     # Check if there's no space
-            space = space.isspace()
-            if space == 0:
-                print("Space OK")
-                break
-            elif space == 0:
+        for space_car in list_password:         # Check if there's a space in the password
+            space_car = space_car.isspace()
+            if space_car == 0:
                 continue
-            else:
-                print("Space not valid, try again")
+            elif space_car == 1:
+                print("No space allowed, try again")
                 user_input()
+            else:
+                break
 
-        for maj in list_password:       # Check if there's at least 1 Maj Character
-            maj = maj.isupper()
-            if maj == 1:
-                print("Maj OK")
-                break
-            elif maj == 0:
-                continue
-            else:
-                print("Maj not valid, try again")
-                user_input()
+        if not any(car_upper in valid_car_upper for car_upper in list_password):
+            print("Upper not OK")
+            user_input()
 
-        for low in list_password:       # Check if there's at least 1 Low Character
-            low = low.islower()
-            if low == 1:
-                print("Low OK")
-                break
-            elif low == 0:
-                continue
-            else:
-                print("Low not valid, try again")
-                user_input()
+        if not any(car_low in valid_car_low for car_low in list_password):
+            print("Low not OK")
+            user_input()
 
-        for digit in list_password:     # Check if there's at least 1 Number
-            digit = digit.isdigit()
-            if digit == 1:
-                print("Digit OK")
-                break
-            elif digit == 0:
-                continue
-            else:
-                print("Digit not valid, try again")
-                user_input()
+        if not any(digit in valid_digit for digit in list_password):
+            print("Digit not OK")
+            user_input()
 
         if any(car_spe in valid_car_spe for car_spe in list_password):      # Check if there's at least 1 Special Character
-            enc_password = (sha256(password.encode('utf-8')).hexdigest())     # Hash the password
+            enc_password = (sha256(password.encode('utf-8')).hexdigest())      # Hash the password and take the json form
 
-            with open("Password.json", "a") as f:       #Open json file to write password and encrypted password
-                f.write("Password :")
-                json.dump(password, f)
-                f.write("\n")
-                f.write("Encrypted Password :")
+            with open("Password.json", "a") as f:       # Open json file to write password and encrypted password
                 json.dump(enc_password, f)
                 f.write("\n")
 
@@ -74,7 +52,7 @@ def user_input():
             user_input()
 
     else:       # If the length is not right, try again
-        print("Password not valid, try again")
+        print("Length not valid, try again")
         user_input()
 
 
