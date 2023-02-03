633296ce89bfca1b19281dc280776ccd18c3a6d4
raphael-rossignol <raphael.rossignol@laplateforme.io>
2023-02-03 13:53:07 +0100

    Added the create_json feature, add passwords to json file feature working, bonus job done

--- Password_V2.py
+++ Password_V2.py
@@ -56,14 +56,35 @@ def encrypt_password():     # If the password is valid it encrypts it and print 
 encrypt_password()
 
 
-def password_json():        # If the password is valid, stores it in Json file and let you assign a name in it
+def create_json():    # Asks the user if there is already a json file named "Password.json", if not creates it else pass
+    if password_checked == True:
+        choice_json = str(input("Do you already have a 'Password.json' file ? Y/N : "))
+        if "Y" in choice_json:
+            pass
+        else:
+            json_dict = {
+                "Password": [
+
+                ]
+            }
+            with open('Password.json', 'w') as f:
+                json.dump(json_dict, f, indent=4)
+
+
+create_json()
+
+
+def password_json(filename='Password.json'):        # If the password is valid, stores it in Json file and let you assign a name in it
     global enc_password
     if password_checked == True:
         password_dict = {
             input("Choose a name for your password : "): enc_password
-        },
-        with open("Password.json", "w") as f:
-            json.dump(password_dict, f, indent=2)
+        }
+        with open(filename, "r+") as f:
+            file_data = json.load(f)
+            file_data["Password"].append(password_dict)
+            f.seek(0)
+            json.dump(file_data, f, indent=2)
 
 
 password_json()
